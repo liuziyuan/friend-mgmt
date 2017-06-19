2143678d5cd9c187485ee0f892a5bbd078687cb3
liuziyuan <liuziyuan.room@gmail.com>
2017-06-19 13:05:38 +0800

    modify

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"friends-mgmt-gin/apis"
 	"friends-mgmt-gin/models"
 	"friends-mgmt-gin/utils"
+	"strings"
+
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
@@ -11,8 +15,9 @@ import (
 
 func main() {
 	g := gin.Default()
+	g.Use(corsHandler)
 
-	g.Static("swagger", "E:/GoWorks/src/friends-mgmt-gin/swagger")
+	g.StaticFS("swagger", http.Dir("swagger"))
 	restapis.Users = g.Group("/api/users")
 	restapis.Friends = g.Group("/api/friends")
 	restapis.Subscriptions = g.Group("/api/subscriptions")
@@ -32,4 +37,16 @@ func main() {
 	// db.Create(&models.User{EmailAddress: "eric"})
 	// db.Create(&models.User{EmailAddress: "terry"})
 	g.Run(":8080")
+}
+
+func corsHandler(c *gin.Context) {
+	fmt.Println("RequestURI :")
+	if strings.HasPrefix(c.Request.RequestURI, "/user") {
+		c.Header("Content-Type", "application/json")
+	}
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("Access-Control-Allow-Credentials", "true")
+	c.Header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization, Pragma, Cache-control, Expires")
+	c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+	c.Header("Access-Control-Max-Age", "1209600")
 }
